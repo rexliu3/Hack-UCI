7b49bbdc39edcc1f3ba40cde933cedbe00c22966
lindama <lindima200@gmail.com>
2021-01-30 01:42:31 -0700

    Implement read from database functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,64 +3,80 @@ import firebase from './firebase/index'
 
 const App = () => {
   const [users, setUser] = useState([])
-  const initState = { firstName: '', lastName: ''}
-  const [inputs, setInputs] = useState(initState)
+  const initstate = { firstName: '', 
+                    lastName: '', 
+                    age: '', 
+                    gender: '', 
+                    phoneNumber: '', 
+                    email: '', 
+                    medicalConditions: '',
+                    wapScore: '',
+                    pastApplications: ''
+}
+  const [inputs, setInputs] = useState(initstate)
 
-  console.log(firebase.db)
+    useEffect( () => {
+        getUsers()
+    }, [])
 
-  useEffect( () => {
-      getUsers()
-  }, [])
-
-  const getUsers = () => {
-    firebase.db.collection('Covid-19 Priority Queue').get()
-      .then(querySnapshot => {
-      querySnapshot.forEach( doc => {
-
-      setUser(prev => ([...prev, doc.data()]))
+    const getUsers = () => {
+      firebase.db.collection('covid-19 vaccine queue').get()
+        .then(querySnapshot => {
+        querySnapshot.forEach( doc => {
+          //prints the data in the database to the console
+          console.log(`${doc.id} => ${JSON.stringify(doc.data())}`)
+          setUser(prev => ([...prev, doc.data()]))
+        })
       })
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-  }
+      .catch(err => {
+        console.log(err.message)
+      })
+    }
 
 
-  const sendUserInfo = async (e) => {
-    e.preventDefault()
-    await firebase.db.collection('Covid-19 Priority Queue').add(inputs)
-    .then( async documentReference => {
-      console.log('user ID', documentReference.id)
-      await setUser([])
+    const sendUserInfo = async (e) => {
+      e.preventDefault()
+      await firebase.db.collection('covid-19 vaccine queue').add(inputs)
+      .then( async documentReference => {
+        console.log('User ID', documentReference.id)
+        await setUser([])
 
-      getUsers()
-    })
-    .catch(error => {
-      console.log(error.message)
-    })
-
+        getUsers()
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
   }
 
   const handleChange = e => {
     const {name, value} = e.target
-    setInputs(prev => ( {...prev, [name]: value} ))
+    setInputs(prev => ({...prev, [name]: value}))
   }
+
+  // const readData = () => {
+  //   firebase.dbdb.collection("queue").get().then((querySnapshot) => {
+  //     querySnapshot.forEach((doc) => {
+  //         console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+  //     });
+  //   });
+  // }
 
   return (
     <div>
-      <h1>Submit User Info</h1>
+      <h1>Covid-19 Vaccine Application</h1>
         <form onSubmit={sendUserInfo}>
-          <input name='First Name'
+          <input name='firstName'
             placeholder="John" 
             value={inputs.firstName}
             onChange={handleChange}/>
           <input 
-            name='Last Name'
+            name='lastName'
             value={inputs.lastName} 
             placeholder="Smith" 
             onChange={handleChange}/>
             <button>Submit</button>
         </form>
+        {/* {users.length === 0 ? <p></p> : users.map(user => console.log(user.firstName)) } */}
     </div>
   );
 };
