57a0d1b0b66ccf4752f15fe056de071009427beb
Anton Chen <contact@antonchen.ca>
2021-01-31 00:36:35 -0700

    Add interactive html pages

--- backend/heatmap/heatmap.py
+++ backend/heatmap/heatmap.py
@@ -1,7 +1,9 @@
 from urllib.request import urlopen
 import json
 import sys
+
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 import pandas as pd
 
@@ -11,9 +13,12 @@ class Heatmap:
 
     def __init__(self):
         
-        self.color_scheme = "Sunsetdark"
+        self.color_scheme = "Blues"
         self.pvi_endpoint_prefix = "https://raw.githubusercontent.com/COVID19PVI/data/master/Model11.2/Model_11.2_"
         self.pvi_endpoint_suffix = "_results.csv"
+
+        self.start_date = datetime(2021, 1, 1)
+        self.end_date = datetime(2021, 1, 29)
 
         self.us_state_abbrev_map = {
             'Alabama': 'AL',
@@ -74,40 +79,45 @@ class Heatmap:
             'Wyoming': 'WY'
         }
 
-    def get_batch_pvi(self):
+    def get_batch_pvi(self, start_date, end_date):
         res = pd.DataFrame(columns=["State", "PVI", "Day"])
 
-        start_date = datetime(2021, 1, 1)
-        end_date = datetime(2021, 1, 29)
-
+        cur_date = start_date
         i = 1
-        while start_date < end_date:
-            cur_df = self.get_pvi_df(start_date.strftime("%Y%m%d"))
+
+        while cur_date < end_date:
+            cur_df = self.get_pvi_df(cur_date.strftime("%Y%m%d"))
             cur_df["Day"] = i
 
             res = pd.concat([res, cur_df], ignore_index=False)
 
-            start_date += timedelta(1)
+            cur_date += timedelta(1)
             i += 1
 
         return res
 
 
-    # Shows heatmap with latest available data.
-    def get_heatmap(self):
-        df = self.get_batch_pvi()
+    # Saves heatmap as an interactive html for the specified month of the start date.
+    def get_heatmap(self, start_date):
+        df = self.get_batch_pvi(start_date, start_date + relativedelta(months=+1) - timedelta(1))
 
         fig = px.choropleth(df, locations=df["State"].tolist(), locationmode="USA-states", 
                 color_continuous_scale=self.color_scheme,
                 animation_frame=df["Day"].tolist(),
                 color=df["PVI"].tolist(), 
+                hover_name=df["State"].tolist(), 
                 scope="usa",
+                labels={
+                    "color": "PVI",
+                    "locations": "State",
+                    "animation_frame": "Day"
+                },
                 range_color=(0.2, 0.6),
-                title="Pandemic Vulnerability Index - {}".format(date)
+                title="Pandemic Vulnerability Index - {}".format(start_date.strftime("%B %Y"))
         )
 
         fig.update_layout(transition = {'duration': 10})
-        fig.show()
+        fig.write_html("interactive/heatmap-{}.html".format(start_date.strftime("%Y-%m")))
 
 
     # Get PVI data from NIEHS, parse data into format ["PVI", "State Initials"].
@@ -140,14 +150,9 @@ class Heatmap:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        print("ERROR: Too many arguments! Format: $ python3 heatmap.py <YYYYMMDD>")
+    hm = Heatmap()
 
-    else:
-        hm = Heatmap()
-
-        date = "20210129"
-        if len(sys.argv) > 1:
-            date = sys.argv[1]
-
-        hm.get_heatmap()
+    covid_start_date = datetime(2020, 3, 1)
+    for i in range(11):
+        hm.get_heatmap(covid_start_date + relativedelta(months=+i))
+        print(i + 1)
