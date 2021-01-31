ceb37392256163dc206b9baa28395df0c143f547
lindama <lindima200@gmail.com>
2021-01-30 17:18:24 -0700

    Add .env file and ui changes

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { navigate, Redirect } from "@reach/router"
 import './LandingPage.css';
 import { Button } from "shards-react";
 import backgroundImg from '../images/covid-19-coronavirus-lab.jpg'
+import logo from '../images/logo.jpg'
 
 const LandingPage = () => {
     const NavBar = () => (
         <header className='navbar'>
-            <div className='title item'>Vaccelerate</div>
-            <div className='item'>About Us</div>
-            <div className='item'>Contact</div>
-            <div className='item'>Help</div>  
-            <Button className='navButton'>Login</Button>      
+            <div className='title logo'>
+                <img style={{top:"1px", width:"10em", height:"2em", padding:"1px 1px"}} src={logo}></img>
+            </div>
+            {/* <div className='title item'>Vaccelerator</div> */}
+            <div className='item'>
+                <a style={{textDecoration:'none'}} href="/faq">Contact</a>
+            </div>
+            <div className='item'>
+                <a style={{textDecoration:'none'}} href="/faq">Help</a>
+            </div>  
+            <div className='item'>
+                <a style={{textDecoration:'none'}} href="/education">Resources</a>
+            </div>
+            <Button className='navButton' href="/user-profile-lite">Login</Button>      
         </header>
     )
     
@@ -19,11 +28,11 @@ return (
     <div>
         <container>
             <NavBar />
-            <p>
-                <h1 className='bigTitle'>Vaccelerate</h1>
-                <h5 className='description'>Informing Data Driven Vaccine Distribution</h5>
-                <Button href='/dashboard' className='dashboard'> To Dashboard</Button>
-            </p>
+            <container>
+                <h1 className='bigTitle'>Vaccelerator</h1>
+                <h5 className='description'>Informed Data Driven Vaccine Distribution</h5>
+                <Button href='/dashboard' size='lg' className='dashboard'>Go To Dashboard</Button>
+            </container>
             <img className='backgroundImg' src={backgroundImg} alt='covid-19 vaccine research'></img>
         </container>
     </div>
