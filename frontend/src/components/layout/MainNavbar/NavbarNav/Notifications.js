b10fc7754be736673cda5fc08dd6f6f8bd619da3
lindama <lindima200@gmail.com>
2021-01-30 20:43:19 -0700

    tidy up frontend

--- frontend/src/components/layout/MainNavbar/NavbarNav/Notifications.js
+++ frontend/src/components/layout/MainNavbar/NavbarNav/Notifications.js
@@ -36,36 +36,6 @@ export default class Notifications extends React.Component {
           open={this.state.visible}
           className="dropdown-menu dropdown-menu-small"
         >
-          <DropdownItem>
-            <div className="notification__icon-wrapper">
-              <div className="notification__icon">
-                <i className="material-icons">&#xE6E1;</i>
-              </div>
-            </div>
-            <div className="notification__content">
-              <span className="notification__category">Analytics</span>
-              <p>
-                Your website’s active users count increased by{" "}
-                <span className="text-success text-semibold">28%</span> in the
-                last week. Great job!
-              </p>
-            </div>
-          </DropdownItem>
-          <DropdownItem>
-            <div className="notification__icon-wrapper">
-              <div className="notification__icon">
-                <i className="material-icons">&#xE8D1;</i>
-              </div>
-            </div>
-            <div className="notification__content">
-              <span className="notification__category">Sales</span>
-              <p>
-                Last week your store’s sales count decreased by{" "}
-                <span className="text-danger text-semibold">5.52%</span>. It
-                could have been worse!
-              </p>
-            </div>
-          </DropdownItem>
           <DropdownItem className="notification__all text-center">
             View all Notifications
           </DropdownItem>
